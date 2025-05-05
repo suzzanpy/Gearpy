6c525621cb93f2995c9a47d566345e417a8ab286
suzzanpy <suzankafle56@gmail.com>
2025-05-05 08:28:20 +0545

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gearpy",
+    version="0.1.0",
+    author="Your Name",
+    description="Auto-compiles and parallelizes CPU-bound Python code using a simple decorator.",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
